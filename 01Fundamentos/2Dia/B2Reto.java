7df8eba720fcfdf15e4dabc3d085ff53776c22cf
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 19:28:58 -0500

    Practica Validacion Numero(Negativo, Positivo O Cero)

---
+++ 01Fundamentos/2Dia/B2Reto.java
@@ -0,0 +1,26 @@
+
+public class B2Reto {
+
+    public static void main(String[] args) {
+
+        // Definimos Las Variables
+        int numero = 0;
+
+        // Validamos Si El Numero Es Positivo, Negativo O Cero
+        if (numero > 0) {
+
+            System.out.println("Positivo");
+
+        } else if (numero < 0) {
+
+            System.out.println("Negativo");
+
+        } else {
+
+            System.out.println("Cero");
+
+        }
+
+    }
+
+}
