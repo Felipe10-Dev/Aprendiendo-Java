7ef85b7184413b46326c49331b4408df5242e448
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 14:56:39 -0500

    Practica Recorrido Y Suma De Los Numeros Del 1 Al 5

---
+++ 01Fundamentos/3Dia/C5Reto.java
@@ -0,0 +1,22 @@
+
+public class C5Reto {
+
+    public static void main(String[] args) {
+
+        // Definimos Variables
+        int suma = 0;
+
+        // Estructura Repetitiva Que Va A Sumar Los Numeros Entre El 1 Al 5
+        for (int i = 1; i <= 5; i++) {
+
+            // Suma Y Asignacion
+            suma += i;
+
+        }
+
+        // Imprimimos 
+        System.out.println("Suma = " + suma);
+
+    }
+
+}
