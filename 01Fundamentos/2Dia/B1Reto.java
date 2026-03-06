4c962fb066da8a1bc6f26fa9ec5fe6cd14cdd38c
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 19:27:39 -0500

    Practica Validacion De Edad

---
+++ 01Fundamentos/2Dia/B1Reto.java
@@ -0,0 +1,21 @@
+
+public class B1Reto {
+
+    public static void main(String[] args) {
+
+        // Definimos Las Variables
+        int edad = 10;
+
+        // Validacion De La Edad De Un Bar
+        if (edad >= 18) {
+
+            System.out.println("Puede Entrar Al Bar");
+
+        } else {
+
+            System.out.println("No Puede Entrar Al Bar");
+
+        }
+    }
+
+}
