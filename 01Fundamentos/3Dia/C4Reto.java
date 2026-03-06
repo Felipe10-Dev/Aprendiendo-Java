9db6cabe34ced6ffb8c91d535b414704889f781b
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 14:56:02 -0500

    Practica Impresion Numeros Del 1 Al 5

---
+++ 01Fundamentos/3Dia/C4Reto.java
@@ -0,0 +1,20 @@
+
+public class C4Reto {
+
+    public static void main(String[] args) {
+
+        // Definimos Las Variables
+        int i = 1;
+
+        // Mediante Una Estructura Repetitiva While
+        while (i <= 5) {
+
+            // Imprimimos
+            System.out.println(i);
+            i++;
+
+        }
+
+    }
+
+}
