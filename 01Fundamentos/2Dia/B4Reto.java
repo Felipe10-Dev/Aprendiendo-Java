54548e57a80453104326d324e13a1adb7514bd01
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 19:31:53 -0500

    Practica Validacion y/o Eleccion Dia Semana

---
+++ 01Fundamentos/2Dia/B4Reto.java
@@ -0,0 +1,46 @@
+
+public class B4Reto {
+
+    public static void main(String[] args) {
+
+        // Definimos Variables
+        int dia = 4;
+
+        // Estructura De Control Para Diferentes Opciones
+        switch (dia) {
+
+            case 1:
+                System.out.println("Domingo");
+                break;
+
+            case 2:
+                System.out.println("Lunes");
+                break;
+
+            case 3:
+                System.out.println("Martes");
+                break;
+
+            case 4:
+                System.out.println("Miercoles");
+                break;
+
+            case 5:
+                System.out.println("Jueves");
+                break;
+
+            case 6:
+                System.out.println("Viernes");
+                break;
+
+            case 7:
+                System.out.println("Sabado");
+                break;
+
+            default:
+                System.out.println("Opcion Invalida");
+        }
+
+    }
+
+}
