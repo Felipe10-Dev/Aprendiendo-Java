9a278cf7fbecb6b6429ea4a2e6fd01fd4a507eb8
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 14:53:38 -0500

    Practica Impresion De Numeros Pares Del 1 Al 20

---
+++ 01Fundamentos/3Dia/C3Reto.java
@@ -0,0 +1,20 @@
+
+public class C3Reto {
+
+    public static void main(String[] args) {
+
+        // Estructura Repetitiva Para Imprimir Los Pares Entre 1 Y 20 
+        for (int i = 1; i <= 20; i++) {
+
+            // Validamos Si El Numero Es Par
+            if (i % 2 == 0) {
+
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+
+}
