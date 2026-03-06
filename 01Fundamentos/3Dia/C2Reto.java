49c840d4fb9d16726dcfbe3e4c3a20345cb37542
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 14:51:42 -0500

    Practica Impresion Numeros Del 10 Al 1

---
+++ 01Fundamentos/3Dia/C2Reto.java
@@ -0,0 +1,15 @@
+
+public class C2Reto {
+
+    public static void main(String[] args) {
+
+        // Mediante Una Estructura Repetitiva Imprimimos Los Numeros Del 10 Al 1 De Forma Ascendente
+        for (int i = 10; i >= 1; i--) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
