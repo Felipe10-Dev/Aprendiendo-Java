ca394914d743478ae23c6d6e97142830a47732fe
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-07 23:46:59 -0500

    Practica Vaidar Si El Usuario Tiene Permitida La Entrada

---
+++ 01Fundamentos/3Dia/C6Reto.java
@@ -0,0 +1,18 @@
+
+public class C6Reto {
+
+    public static void main(String[] args) {
+
+        // Definimos Variables
+        boolean isPermitido = true;
+
+        // Ciclo Repetitivo Para Verificar Si Tiene La Entrada Permitida Pa
+        do {
+
+            System.out.println("Entrada Permitida");
+
+        } while (isPermitido == true);
+
+    }
+
+}
