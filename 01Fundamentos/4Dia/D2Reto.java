d5508240e5e8708fd8da8593ca49d8d915837050
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-09 17:57:08 -0500

    Practica Creacion Y Impresion Del Primer Y Ultimo Numero Del Array

---
+++ 01Fundamentos/4Dia/D2Reto.java
@@ -0,0 +1,17 @@
+
+public class D2Reto {
+
+    public static void main(String[] args) {
+
+        // Creammos Un Array Con 10 Posiciones 
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // mostramos / Imprimimos Solo El Primer Y Ultimo Numero
+        // Primer Numero
+        System.out.println("Primer Numero = " + numeros[0]);
+
+        // Ultimo Numero
+        System.out.println("Ultimo Numero = " + numeros[9]);
+    }
+
+}
