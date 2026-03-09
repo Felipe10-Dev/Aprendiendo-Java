c438fd21ae5c54e459e9817ef52f53e0c1146ea8
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-09 17:55:56 -0500

    Practica Creacion Y Recorrido De Un Array

---
+++ 01Fundamentos/4Dia/D1Reto.java
@@ -0,0 +1,17 @@
+
+public class D1Reto {
+
+    public static void main(String[] args) {
+
+        // Creacion De Array Con Sus Respectivos Datos
+        int[] numeros = {5, 10, 15, 20, 25};
+
+        // Recorremos Nuestro Arreglo Mediante Un Ciclo For
+        for (int i = 0; i < numeros.length; i++) {
+
+            System.out.println(numeros[i]);
+
+        }
+    }
+
+}
