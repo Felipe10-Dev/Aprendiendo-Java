1104f70c3fa3ace36eeef46751e93084ef50b574
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-10 11:11:09 -0500

    Practica Creacion Y Llamado De Un Metodo

---
+++ 01Fundamentos/5Dia/E1Reto.java
@@ -0,0 +1,17 @@
+
+public class E1Reto {
+
+    public static void main(String[] args) {
+
+        saludar(); // llamamos El Metodo
+
+    }
+
+    // Metodos -> Saludar
+    public static void saludar() {
+
+        System.out.println("Bienvenido Al Programa");
+
+    }
+
+}
